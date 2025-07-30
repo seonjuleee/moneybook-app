486e604715496c20461af52ab53cc71b85e9a0a2
seonjuleee <nlsj0820@naver.com>
2025-07-30 23:02:22 +0900

    Initial Commit

---
+++ ai-money-book/ai_money_bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_money_bookApp.swift
+//  ai-money-book
+//
+//  Created by Seonju Lee on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ai_money_bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ai-money-bookTests/ai_money_bookTests.swift
@@ -0,0 +1,16 @@
+//
+//  ai_money_bookTests.swift
+//  ai-money-bookTests
+//
+//  Created by Seonju Lee on 7/30/25.
+//
+
+import Testing
+
+struct ai_money_bookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
